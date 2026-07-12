b32ae444c80bc124fa76efbd21170022395c4647
h32914667-gif <h32914667@gmail.com>
2026-07-12 20:53:10 +0300

    Fix CORS: handle OPTIONS in middleware

--- server/server.js
+++ server/server.js
@@ -32,11 +32,14 @@ if (!process.env.YANDEX_API_KEY) {
 
 const app = express();
 
-// ===== CORS =====
+// ===== CORS (обработка preflight OPTIONS) =====
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 });
 
