1d9c53b2a641bf3f36ded9b52203b49a099c14b3
h32914667-gif <h32914667@gmail.com>
2026-07-12 20:49:48 +0300

    Fix Express 5: remove app.options('*')

--- server/server.js
+++ server/server.js
@@ -32,19 +32,12 @@ if (!process.env.YANDEX_API_KEY) {
 
 const app = express();
 
-// ===== ГАРАНТИРОВАННЫЙ CORS (включая OPTIONS) =====
+// ===== CORS =====
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
-});
-
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.sendStatus(200);
 });
 
 app.use(express.json());
